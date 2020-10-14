2fc388c00c974552629b0591fe824f2f6fceb6eb
solbencas <cecibenitezca@gmail.com>
2020-10-14 11:40:21 -0300

    Add helpers methods to views

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,9 @@
 module TweetsHelper
+  def anonymous_user?(tweet)
+    user_signed_in? && (current_user.id == tweet.user_id || current_user.members_only? || current_user.admin?)
+  end
+
+  def current_user_signed_in?(tweet)
+    user_signed_in? && current_user.id == tweet.user_id
+  end
 end
